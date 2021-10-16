8cca0acfbb7918ec864f3536c66b656442a4271d
hoa.nguyen <hoa.nguyen@opswat.com>
2021-10-17 00:02:35 +0700

    credit app

--- src/App.js
+++ src/App.js
@@ -5,12 +5,21 @@ import { HomePage } from "./Components/HomePage/HomePage";
 import "./assets/css/App.css";
 import { TimeSheet } from "./Components/TimeSheet/TimeSheet";
 import { DataProvider } from "./Provider/DataProvider";
-import Footer from 'rc-footer';
-import 'rc-footer/assets/index.css';
+import { Button, Space } from "antd";
+import { GithubFilled, FacebookFilled, YoutubeFilled } from '@ant-design/icons';
+
 // https://www.svgbackgrounds.com/
 // https://www.flaticon.com/
 
 function App() {
+
+  const redirect_blank = (url) => {
+    var a = document.createElement('a');
+    a.target = "_blank";
+    a.href = url;
+    a.click();
+  }
+
   return (
     <div className="full">
       <DataProvider>
@@ -20,22 +29,14 @@ function App() {
         </HashRouter>
       </DataProvider>
 
-      <Footer
-        columns={[
-          // {
-          //   icon: (
-          //     <img src="https://img.icons8.com/nolan/64/github.png" />
-          //   ),
-          //   title: 'Github',
-          //   url:"https://github.com",
-          //   description: 'github',
-          //   openExternal: true,
-            
-          // }
-        ]}
-        bottom="Copyright @2021 ❤️ by hhpr98"
-        style={{ color: "white", marginTop: 100 }}
-      />
+      <li className="footer-flex">
+        <ul><Button shape="circle" icon={<GithubFilled />} size="large" onClick={() => redirect_blank("https://github.com/hhpr98")} /></ul>
+        <ul><Button shape="circle" icon={<FacebookFilled />} size="large" style={{ color: "#4267b2" }} onClick={() => redirect_blank("https://www.facebook.com/nguyenhuuhoa.15.04.1998")} /></ul>
+        <ul><Button shape="circle" icon={<YoutubeFilled />} size="large" style={{ color: "#EA4335" }} onClick={() => redirect_blank("https://www.youtube.com/channel/UCu4l-eJrIEknLQBjhRR_qDw")} /></ul>
+      </li>
+
+      <div className="copyright">Copyright @2021. From Ton Duc Thang with love ❤️</div>
+
     </div>
   );
 }
