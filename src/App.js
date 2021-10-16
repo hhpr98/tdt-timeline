f478df20bf563a2a7b527c7117e303de9432548d
hoa.nguyen <hoa.nguyen@opswat.com>
2021-10-16 16:40:54 +0700

    define layout

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from "react";
 import "antd/dist/antd.css";
 import { HashRouter, Route } from "react-router-dom";
 import { HomePage } from "./Components/Home";
+import "./assets/css/App.css";
+// https://www.svgbackgrounds.com/
 
 function App() {
   return (
-    <HashRouter>
-      <Route exact path="/" component={HomePage} />
-    </HashRouter>
+    <div className="test">
+      <HashRouter>
+        <Route exact path="/" component={HomePage} />
+      </HashRouter>
+    </div>
   );
 }
 
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,12 +1,48 @@
 import { Button } from "antd";
+import {Table} from "antd";
 
 export const HomePage = (props) => {
+    const dataSource = [
+        {
+            key: '1',
+            name: 'Mike',
+            age: 32,
+            address: '10 Downing Street',
+        },
+        {
+            key: '2',
+            name: 'John',
+            age: 42,
+            address: '10 Downing Street',
+        },
+    ];
+
+    const columns = [
+        {
+            title: 'Name',
+            dataIndex: 'name',
+            key: 'name',
+        },
+        {
+            title: 'Age',
+            dataIndex: 'age',
+            key: 'age',
+        },
+        {
+            title: 'Address',
+            dataIndex: 'address',
+            key: 'address',
+        },
+    ];
+
     return (
         <div>
-            <div>Hello World!</div>
-            <Button type="primary" loading={true} onClick={() => alert("huhu")}>
+            <Button type="primary" loading={false} onClick={() => alert("huhu")}>
                 Click me!
             </Button>
+
+
+            <Table dataSource={dataSource} columns={columns} />
         </div>
     );
 }
